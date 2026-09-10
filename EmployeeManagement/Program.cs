52802cad349667d8c6328a418a0c44241fece5b3
darin74 <darinluiz@gmail.com>
2026-09-11 00:53:34 +0200

    commit för senaste ändringar

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -4,12 +4,46 @@
     {
         static void Main(string[] args)
         {
+            List<string> employees = new List<string>();
+
+            bool running = true;
+
+            while (running)
+            {
             Console.WriteLine("=== Employee Management ===");
             Console.WriteLine("1. Add employee");
             Console.WriteLine("2. View employees");
             Console.WriteLine("3. Search employee");
             Console.WriteLine("4. Remove employee");
             Console.WriteLine("5. Exit");
+
+            Console.Write("Choose an option: ");
+            string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    Console.Write("Enter employee name: ");
+                    string name = Console.ReadLine();
+
+
+                    Console.WriteLine("Employee added: " + name);
+                    employees.Add(name);
+                }
+                if (choice == "2")
+                {
+                    foreach (string employee in employees)
+                    {
+                        Console.WriteLine(employee);
+                    }
+                }
+
+                if (choice == "5")
+                {
+                    running = false;
+                }
+            
+            }
+
         }
     }
 }
